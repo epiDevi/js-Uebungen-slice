a90913d933c5104d2dfc5b1c896cbb937ed89176
SuperCoder <supercoder@AirvonSperCoder.local>
2023-09-26 13:20:39 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const lieblingsreiseziele= ["Schweden", "Polen", "Italien" ,"Norwegen", "Griechenland"];
+console.log(lieblingsreiseziele);
+
+const newArr= lieblingsreiseziele.slice(1,3);
+console.log(newArr);
+console.log(lieblingsreiseziele);
